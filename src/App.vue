eb0f0c1c02a8fd29a69fca3e36b617978acc44ed
Gülistan Boylu <gulistan.boylu@gulistans-mbp.home>
2019-01-08 12:30:42 +0000

    Create navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <p>This will be an awesome project!</p>
+    <div class="content">
+      <div class="logo-container">
+        <a href="">
+          <img src="src/assets/images/logo.png" alt="">
+        </a>
+        <nav>
+          <a href="">Up Coming</a>
+          <a href="">Now Playing</a>
+          <a href="">Top Rated</a>
+        </nav>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,9 +33,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
-
+body {
+  margin: 0;
+}
 h1, h2 {
   font-weight: normal;
 }
@@ -42,4 +55,31 @@ li {
 a {
   color: #42b983;
 }
+.logo-container {
+    display: flex;
+    img {
+      height: 80px;
+    }
+  }
+  .content {
+    background-color: #000;
+  }
+  nav {
+    align-items: center;
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    a {
+      color: white;
+      font-weight: 500;
+      text-decoration: none;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+      margin: 0 30px;
+      transition: color .4s linear;
+      &:hover {
+        color: #ed635d;
+      }
+    }
+  }
 </style>

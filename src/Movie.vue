bb67805677a5f7f5de5b84ba4963ceb653a26ed1
Gülistan Boylu <gulistan.boylu@gulistans-mbp.home>
2019-01-09 12:07:28 +0000

    Limit movie overview with 20 words

--- src/Movie.vue
+++ src/Movie.vue
@@ -1,23 +1,65 @@
 
+import func from './vue-temp/vue-editor-bridge';
 <template>
   <div>
     <div>
       <img :src="'https://image.tmdb.org/t/p/w200' + this.movie.poster_path">
     </div>
-    <div>
-      <h1>{{ this.movie.title }}</h1>
+    <div class="movie-content">
       <h2>{{ this.movie.title }}</h2>
-      <p>{{ this.movie.overview }}</p>
-      <h3>{{this.movie.release_date}}</h3>
+      <p>{{ truncate }}</p>
+      <h4>{{this.movie.release_date}}</h4>
+      <button v-on:click="detail()">More Info</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["movie"]
+  props: ["movie"],
+  methods: {
+    detail: function() {
+      alert("You will see the movie detail soon!");
+    }
+  },
+
+  computed: {
+    truncate: function() {
+      return (
+        this.movie.overview
+          .split(" ")
+          .splice(0, 20)
+          .join(" ") + "..."
+      );
+    }
+  }
 };
 </script>
 
 <style>
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+  grid-gap: 1em;
+}
+
+.item {
+  background: #eaeaea;
+  display: flex;
+  justify-content: center;
+  height: auto;
+  margin-left: 5px;
+  margin-right: 5px;
+  flex: 1 1 200px;
+}
+
+.movie-content {
+  /* float: left; */
+}
+
+@supports (display: grid) {
+  .item {
+    margin: 0;
+  }
+}
 </style>

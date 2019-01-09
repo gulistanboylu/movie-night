3c5f6ca423bf0fd8bdbbfdbdc0a2a97ed64da446
Gülistan Boylu <gulistan.boylu@gulistans-mbp.home>
2019-01-09 15:49:07 +0000

    Add font family

--- src/App.vue
+++ src/App.vue
@@ -49,8 +49,9 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900');
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Source Sans Pro', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: left;
@@ -65,7 +66,7 @@ body {
 .app-container {
   margin: 0 auto;
   max-width: 80em;
-  padding: 2em 0;
+  padding: 2em;
 }
 
 h1,

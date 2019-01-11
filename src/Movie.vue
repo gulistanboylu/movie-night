0910972181e7359e121dba22b29e79c5c765f0da
Gülistan Boylu <gulistan.boylu@gulistans-mbp.home>
2019-01-11 17:31:20 +0000

    Create modal component for movie details

--- src/Movie.vue
+++ src/Movie.vue
@@ -1,90 +1,103 @@
 
 import func from './vue-temp/vue-editor-bridge';
 <template>
+  <div>
     <div>
-        <div>
-            <img :src="'https://image.tmdb.org/t/p/w200' + this.movie.poster_path">
-        </div>
-        <div class="movie-content">
-            <h3>{{ this.movie.title }}</h3>
-            <p>{{ truncate }}</p>
-            <h4>{{this.movie.release_date}}</h4>
-            <button v-on:click="detail()">More Info</button>
-        </div>
+      <img :src="'https://image.tmdb.org/t/p/w200' + this.movie.poster_path">
     </div>
+    <div class="movie-content">
+      <h3>{{ this.movie.title }}</h3>
+      <p>{{ truncate }}</p>
+      <h4>{{this.movie.release_date}}</h4>
+      <button type="button" class="btn" @click="showModal">More Info</button>
+      <modal v-show="isModalVisible" @close="closeModal"/>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ["movie"],
-        methods: {
-            detail: function() {
-                alert("You will see the movie detail soon!");
-            }
-        },
-    
-        computed: {
-            truncate: function() {
-                return (
-                    this.movie.overview
-                    .split(" ")
-                    .splice(0, 20)
-                    .join(" ") + "..."
-                );
-            }
-        }
+import Modal from "./Modal.vue";
+export default {
+  components: {
+    modal: Modal
+  },
+  data() {
+    return {
+      isModalVisible: false
     };
+  },
+  props: ["movie"],
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    }
+  },
+
+  computed: {
+    truncate: function() {
+      return (
+        this.movie.overview
+          .split(" ")
+          .splice(0, 20)
+          .join(" ") + "..."
+      );
+    }
+  }
+};
 </script>
 
 <style>
-    .container {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
-        grid-gap: 3em;
-    }
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+  grid-gap: 3em;
+}
 
-    h1 {
-        color: black;
-        font-weight: 600;
-        margin: 1em 0 1.5em 0;
-    }
-    
-    .item {
-        background: #ffffff;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-        display: flex;
-        flex: 1 1 200px;
-        height: auto;
-        justify-content: center;
-        margin-left: 5px;
-        margin-right: 5px;
-    }
-    
-    .movie-content {
-        padding: 0 1.5em;
-    }
-    
-    h4 {
-        color: #868686;
-        margin: 2em 0;
-    }
-    
-    button {
-        background-color: #ed635d;
-        color: white;
-        font-family: 'Source Sans Pro', sans-serif;
-        font-size: 0.9em;
-        letter-spacing: 1;
-        letter-spacing: 0.1em;
-        text-transform: uppercase;
-        font-weight: 600;
-        padding: 8px 20px;
-        transition: color 0.4s linear;
-    }
-    
-    @supports (display: grid) {
-        .item {
-            margin: 0;
-        }
-    }
+h1 {
+  color: black;
+  font-weight: 600;
+  margin: 1em 0 1.5em 0;
+}
+
+.item {
+  background: #ffffff;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex: 1 1 200px;
+  height: auto;
+  justify-content: center;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+.movie-content {
+  padding: 0 1.5em;
+}
+
+h4 {
+  color: #868686;
+  margin: 2em 0;
+}
+
+button {
+  background-color: #ed635d;
+  color: white;
+  font-family: "Source Sans Pro", sans-serif;
+  font-size: 0.9em;
+  letter-spacing: 1;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  font-weight: 600;
+  padding: 8px 20px;
+  transition: color 0.4s linear;
+}
+
+@supports (display: grid) {
+  .item {
+    margin: 0;
+  }
+}
 </style>

b63e44a20da8632a3a7fa94529494f4193f22f14
Gülistan Boylu <gulistan.boylu@gulistans-mbp.home>
2019-01-23 15:54:09 +0000

    Create search component

---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template lang='pug'>
+  div.search
+    input(v-model="message" placeholder="Seach for movie")
+    p {{ message }}
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang='scss'>
+    input {
+        width: 100%;
+        padding: 20px 30px;
+        text-transform: uppercase;
+        font-style: italic;
+        color: grey;
+        font-size: 1em;
+        border:#d1d1d1 2px solid;
+    }
+</style>
